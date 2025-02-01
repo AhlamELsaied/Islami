742b75770ce91a2973815b57e4c141f99c1b02bb
Ahlam ElSaied <ahlam.elsaiiied22@gmail.com>
2025-02-01 11:01:07 +0200

    AzanScreen add PrayTime add MorningAzkar add EveningAzkar

--- lib/screen/home_screen/azan/widget/widget.dart
+++ lib/screen/home_screen/azan/widget/widget.dart
@@ -1,185 +1,85 @@
 import 'package:flutter/material.dart';
 import 'package:islami/core/theme/app_assets.dart';
 import 'package:islami/core/theme/app_color.dart';
+import 'package:islami/screen/home_screen/azan/widget/prayCard.dart';
+import 'package:islami/screen/home_screen/azan/widget/prayTime.dart';
+import 'package:islami/screen/home_screen/azan/widget/widget.dart';
 
 class PrayCard extends StatelessWidget {
   final PrayTime prayer;
-
-  const PrayCard({super.key, required this.prayer});
+  const PrayCard({super.key,required this.prayer});
 
   @override
   Widget build(BuildContext context) {
-    return Stack(children: [
-      Container(
+    return ListView(
+        shrinkWrap: true,
+      children: [Container(
+        padding: const EdgeInsets.all(16),
+        height: MediaQuery.of(context).size.height * 0.27,
         width: MediaQuery.of(context).size.width * 0.9,
-        height: MediaQuery.of(context).size.height * 0.25,
         decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(20), color: AppColor.DarkGold),
-      ),
-      const Align(
-        alignment: Alignment.topRight,
-        child: Padding(
-          padding: EdgeInsets.all(8.0),
-          child: Text(
-            " 09 Muh\n 1446",
-            style: TextStyle(color: AppColor.white, fontSize: 14),
-            textAlign: TextAlign.right,
-          ),
-        ),
-      ),
-      Stack(
-        children: [Image.asset(
-          AppAssets.backgroundCard,
-          width: MediaQuery.of(context).size.width * 0.9,
-          height: MediaQuery.of(context).size.height * 0.25,
-          fit: BoxFit.cover,
-        ),
-          const Align(
-            alignment: Alignment.topCenter,
-            child: Padding(
-              padding: EdgeInsets.all(8.0),
-              child: Text(
-                "Pray Time \n  Tuesday",
-                style: TextStyle(color: AppColor.Black, fontSize: 14,fontWeight: FontWeight.bold),
-                textAlign: TextAlign.left,
-              ),
-            ),
-          ),
-    ]
-      ),
+            color: AppColor.DarkGold,
+            borderRadius: BorderRadius.circular(30),
+            image: const DecorationImage(
+                image: AssetImage(AppAssets.backgroundCard), fit: BoxFit.cover)),
+        child: Column(
+          children: [
+            Column(
+              children: [
+                const Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "16 Jul,\n2024",
+                      style: TextStyle(color: AppColor.white, fontSize: 16),
+                      textAlign: TextAlign.center,
+                    ),
+                    Text(
+                      "Pray Time\nTuesday",
+                      style: TextStyle(
+                          color: AppColor.Black,
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold),
+                      textAlign: TextAlign.center,
+                    ),
+                    Text(
+                      "09 Muh,\n1446",
+                      style: TextStyle(color: AppColor.white, fontSize: 16),
+                      textAlign: TextAlign.center,
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.11,
+                  child: ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                      itemCount: 5,
+                    itemBuilder: (context, index) => PraycardTime(prayer: PrayTime.prayers[index]),
+                  ),
+                ),
+                const SizedBox(height:20,),
 
-      const Align(
-        alignment: Alignment.topLeft,
-        child: Padding(
-          padding: EdgeInsets.all(8.0),
-          child: Text(
-            "16 Jul,\n 2024",
-            style: TextStyle(color: AppColor.white, fontSize: 14),
-            textAlign: TextAlign.left,
-          ),
+                Row(
+                  children: [
+                    const Expanded(
+                      child: Text(
+                                   "Next Pray - 02:32",
+                        style: TextStyle(color: AppColor.Black, fontSize: 14),textAlign: TextAlign.center,
+                      ),
+                    ),
+                    Image.asset(AppAssets.earphone,color: AppColor.Black,fit: BoxFit.cover,),
+                  ],
+                )
+              ],
+            ),
+          ],
         ),
       ),
-      Align(
-        alignment: Alignment.bottomCenter,
-        child: Container(
-          width: MediaQuery.of(context).size.width * 0.9,
-          height: MediaQuery.of(context).size.height * 0.15,
-          margin: const EdgeInsets.symmetric(horizontal: 6, vertical: 40),
-          decoration: BoxDecoration(
-            color: Colors.black87,
-            borderRadius: BorderRadius.circular(15),
-          ),
-          child: const Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    "Fajr",
-                    style: TextStyle(
-                        color: AppColor.white,
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold),
-                  ),
-                  SizedBox(height: 4),
-                  Text(
-                    "09:12 \nPM",
-                    style: TextStyle(color:AppColor.white, fontSize: 14),
-                    textAlign: TextAlign.center,
-                  ),
-                ],
-              ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    "Dhuhr",
-                    style: TextStyle(
-                        color: AppColor.white,
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold),
-                  ),
-                  SizedBox(height: 4),
-                  Text(
-                    "01:01 \nPM",
-                    style: TextStyle(color: AppColor.white, fontSize: 14),
-                    textAlign: TextAlign.center,
-                  ),
-                ],
-              ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    "Asr",
-                    style: TextStyle(
-                        color: AppColor.white,
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold),
-                  ),
-                  SizedBox(height: 4),
-                  Text(
-                    "04:38 \nPM",
-                    style: TextStyle(color: AppColor.white, fontSize: 14),
-                    textAlign: TextAlign.center,
-                  ),
-                ],
-              ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    "Maghrib",
-                    style: TextStyle(
-                        color: AppColor.white,
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold),
-                  ),
-                  SizedBox(height: 4),
-                  Text(
-                    "07:57 \nPM",
-                    style: TextStyle(color: AppColor.white, fontSize: 14),
-                    textAlign: TextAlign.center,
-                  ),
-                ],
-              ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    "Isha",
-                    style: TextStyle(
-                        color: AppColor.white,
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold),
-                  ),
-                  SizedBox(height: 4),
-                  Text(
-                    "09:12 \nPM",
-                    style: TextStyle(color: AppColor.white, fontSize: 14),
-                    textAlign: TextAlign.center,
-                  ),
-                ],
-              ),
-            ],
-          ),
-        ),
-      ),
-    ]);
+    ]
+    );
   }
 }
 
-class PrayTime {
-  final String name;
-  final String time;
-
-  PrayTime({required this.name, required this.time});
-  static List<PrayTime> prayers = [
-    PrayTime(name: "Fajr", time: "09:12 \nPM"),
-    PrayTime(name: "Dhuhr", time: "01:01 \nPM"),
-    PrayTime(name: "Asr", time: "04:38 \nPM"),
-    PrayTime(name: "Maghrib", time: "07:57 \nPM"),
-    PrayTime(name: "Isha", time: "09:12 \nPM"),
-  ];
-}
